76fe5ee7bddff739bd2dca74c34c5afd121a4221
Piyush-Joshi-1002 <piyushjoshi1002@gmail.com>
2025-02-12 16:43:54 -0500

    Added TestUtil file

--- src/test/java/intg/com/learnkafka/library_events_producer/Controller/LibraryEventControllerIntigrationTest.java
+++ src/test/java/intg/com/learnkafka/library_events_producer/Controller/LibraryEventControllerIntigrationTest.java
@@ -1,10 +1,18 @@
 package intg.com.learnkafka.library_events_producer.Controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class LibraryEventControllerIntigrationTest {
+
+
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
     @Test
     void postLibraryEvent(){
 
