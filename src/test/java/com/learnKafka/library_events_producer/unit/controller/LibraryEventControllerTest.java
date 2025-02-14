3d9281ef9149e6c1ec3581098b09e3161aa9f572
Piyush-Joshi-1002 <piyushjoshi1002@gmail.com>
2025-02-14 12:24:43 -0500

    Configure a Kafka consumer and Wire KafkaConsumer and EmbeddedKafkaBroker

--- src/test/java/com/learnKafka/library_events_producer/unit/controller/LibraryEventControllerTest.java
+++ src/test/java/com/learnKafka/library_events_producer/unit/controller/LibraryEventControllerTest.java
@@ -1,21 +1,29 @@
-package com.learnKafka.library_events_producer.controller;
+package com.learnKafka.library_events_producer.unit.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+@WebMvcTest
 class LibraryEventControllerTest {
 
-    @Autowired
-    TestRestTemplate restTemplate;
+
+
     @Test
     void postLibraryEvent() {
 
-        assertEquals(2,2);
+        //given
+
+
+        //when
+
+
+        //then
 
     }
 }
